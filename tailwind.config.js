828bfb52ba8c6639cd0e7b5ff5361ebf1c2d47a0
Faridat Hadassah Mejogbe <faridathadassah4@gmail.com>
2025-05-17 02:13:41 +0700

    Home page updated

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightgray: '#D9D9D9',
+        lightPeach: "#EBDBDB",
+        black: "#2A2826",
+        gray: "#D9D9D9",
+        burntOrange: "#CC5500",
+        white: "#ffffff",
+        brown: "855454",
+       }, 
+       fontFamily: {
+        // Outfit: ['Outfit', 'sans-serif'],
+        // YoungSerif: ['Young Serif', 'serif']
+      },  
+    },
+  },
+  plugins: [],
+}
